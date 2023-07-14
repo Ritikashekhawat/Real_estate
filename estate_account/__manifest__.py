6245504973140380c125cf47c21129b7041b6978
Ritika Shekhawat <rish@odoo.com>
2023-07-14 19:02:32 +0530

    Kanban View

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'Estate Account',
+    'summary': "Estate Account App",
+    'author': "Odoo",
+        
+    'installable': True,
+    'application': True,
+
+    'depends' : ['real_estate',
+                 'account'],
+
+                 'auto_install' : True
+    
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,40 @@
+from odoo import models, fields, Command
+from odoo.exceptions import UserError
+
+
+class EstateProperty(models.Model):
+
+    _name = 'estate.property'
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        print("-----------------------")
+
+        self.env['account.move'].create(
+            {
+                "partner_id": self.buyer_id.id,
+                "move_type": 'out_invoice',
+                # "journal_id": 'account.journal',
+                "invoice_date": fields.Date.today(),
+                "invoice_line_ids": [
+                    Command.create(
+                        {
+                            "name": self.name,
+                            "quantity": 1,
+                            "price_unit": self.selling_price,
+                        }),
+                    Command.create(
+                    {
+                        "name": self.name,
+                        "quantity": 2,
+                        "price_unit": self.selling_price * 0.06,
+                    }),
+                    Command.create({
+
+                        "name": "Administrative fees",
+                        "quantity": 1,
+                        "price_unit": 100.0,
+                    })]
+            }
+        )
+        return super().action_sold()
